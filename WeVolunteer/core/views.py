80b996c46e9a6b400da30997d72a353154206d22
Daniel Oldham <dro@ncbt.org>
2025-07-26 13:53:08 -0600

    fixed data-indicator by updating datastar, added content to boilerplate footer

--- WeVolunteer/core/views.py
+++ WeVolunteer/core/views.py
@@ -1,16 +1,21 @@
 import json
-from datetime import datetime, timedelta
-from time import sleep
+from datetime import datetime
 
-from datastar_py.consts import FragmentMergeMode
+from datastar_py.consts import ElementPatchMode
 from dateutil.relativedelta import relativedelta
 from django.shortcuts import render
 
-from WeVolunteer.utils import respond_via_sse
+from WeVolunteer.utils import respond_via_sse, patch_signals_respond_via_sse
 from core.models import Event
 
 
-def get_events_by_month_and_year(month_year: datetime):
+def get_events_by_month_and_year(month_year: datetime.date):
+    """
+    Get a queryset of events by the given month and year.
+
+    :param month_year: datetime.date containing the desired month and year, day is ignored
+    """
+
     return Event.objects.filter(date__month=month_year.month, date__year=month_year.year).order_by('date', 'start_time')
 
 
@@ -18,6 +23,7 @@ def events(request):
     """
     Render the events page
     """
+
     monthly_events = {}
     now = datetime.today()
     events_date = now
@@ -40,11 +46,19 @@ def events(request):
     return render(request, 'events.html', context)
 
 
-def get_monthly_events_as_sse(request):
+def get_next_month_events_as_sse(request):
+    """
+    Locate the currently displayed month and year from the request datastar dictionary,
+    calculate the subsequent month and year, generate the corresponding events html,
+    and return as an SSE.
+    Also send the incremented month/year back to the frontend as patch signals.
+    """
+
     today = datetime.today()
     month = 0
     year = 0
 
+    # get current month and year from datastar signals dict
     try:
         qdict = json.loads(request.GET.get("datastar", "{}"))
         month = qdict.get("current_month", today.month)
@@ -52,8 +66,18 @@ def get_monthly_events_as_sse(request):
     except TypeError:
         pass
 
+    # increment month by using relativedelta in case the year rolls over
     events_date = datetime(year=year, month=month, day=1)
     events_date = events_date + relativedelta(months=+1)
+
+    # check and see if any future events
+    if not Event.objects.filter(date__gte=events_date).exists():
+        signals = {
+            "more_events": False,
+        }
+
+        return patch_signals_respond_via_sse(signals)
+
     queryset = get_events_by_month_and_year(events_date)
 
     monthly_events = {f"{events_date.strftime('%B')} {events_date.year}": queryset}
@@ -70,4 +94,4 @@ def get_monthly_events_as_sse(request):
         "partials/monthly_event_list.html#monthly-event-list",
         context
     )
-    return respond_via_sse(html_response, signals=signals, selector='#appended-monthly-event-list', merge_mode=FragmentMergeMode.APPEND)
+    return respond_via_sse(html_response, signals=signals, selector='#appended-monthly-event-list', patch_mode=ElementPatchMode.APPEND)
